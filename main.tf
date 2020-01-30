65a9bc8e6a7c09ed16a38d627e0942a5720a66a4
orpere <orlando.capoeiraraiz@gmail.com>
2020-01-30 11:08:13 +0100

    sg

--- main.tf
+++ main.tf
@@ -63,10 +63,4 @@ resource "aws_security_group" "urbackup" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
